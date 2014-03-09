28405b7f410b98c98518425ca502f09d36153c54
theHarvester <james.harvey994@gmail.com>
2014-03-09 13:54:42 +1000

    Refactored route into accounts controller

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -6,11 +6,53 @@ class AccountController extends BaseController {
 	 *
 	 * @return Response
 	 */
-	public function login()
+	public function loginAttempt()
 	{
-    	return View::make('account.login');
+    	$user = array(
+	        'username' => Input::get('username'),
+	        'password' => Input::get('password')
+	    );
+	    
+	    if (Auth::attempt($user)) {
+	        return Redirect::route('home')
+	            ->with('flash_notice', 'You are successfully logged in.');
+	    }
+	    
+	    // authentication failure! lets go back to the login page
+	    return Redirect::route('login')
+	        ->with('flash_error', 'Your username/password combination was incorrect.')
+	        ->withInput();
 	}
 
+	public function loginPage(){
+		return View::make('account.login');
+	}
+
+	public function logout(){
+		Auth::logout();
+
+        return Redirect::route('home')
+            ->with('flash_notice', 'You are successfully logged out.');
+	}
+
+	public function signupAttempt(){
+		$user = array(
+	        'username' => Input::get('username'),
+	        'email' => Input::get('email'),
+	        'password' => Input::get('password')
+	    );
+	        
+	    // if (Auth::attempt($user)) {
+	    //     return Redirect::route('home')
+	    //         ->with('flash_notice', 'You are successfully logged in.');
+	    // }
+	    dd($user);
+	    
+	    // authentication failure! lets go back to the login page
+	    // return Redirect::route('login')
+	    //     ->with('flash_error', 'Your username/password combination was incorrect.')
+	    //     ->withInput();
+	}
 
 
 }
--- app/routes.php
+++ app/routes.php
@@ -23,89 +23,23 @@ Route::get('/', array('as' => 'home', function () {
     }
 }));
 
-/**
-    Login
-*/
 
-Route::get('account/login', array('as' => 'login', function () {
-    return View::make('account.login');
-}))->before('guest');
-
-Route::post('account/login', function () {
-	$user = array(
-        'username' => Input::get('username'),
-        'password' => Input::get('password')
-    );
-    
-    if (Auth::attempt($user)) {
-        return Redirect::route('home')
-            ->with('flash_notice', 'You are successfully logged in.');
-    }
-    
-    // authentication failure! lets go back to the login page
-    return Redirect::route('login')
-        ->with('flash_error', 'Your username/password combination was incorrect.')
-        ->withInput();
+Route::group(array('prefix' => 'account'), function()
+{
+    Route::post('login', 'AccountController@loginAttempt');
+    Route::get('login', array('as' => 'login', 'uses' => 'AccountController@loginPage'))->before('guest');
+    Route::get('logout', array('as' => 'logout', 'uses' => 'AccountController@logout'))->before('auth');
+    Route::post('signup', 'AccountController@signupAttempt');
 });
 
-/**
-    Logout
-*/
 
-Route::get('logout', array('as' => 'logout', function () {
-    Auth::logout();
 
-    return Redirect::route('home')
-        ->with('flash_notice', 'You are successfully logged out.');
-}))->before('auth');
-
-/**
-    Signup
-*/
-
-Route::post('account/signup', function () {
-    $user = array(
-        'username' => Input::get('username'),
-        'email' => Input::get('email'),
-        'password' => Input::get('password')
-    );
-        
-    // if (Auth::attempt($user)) {
-    //     return Redirect::route('home')
-    //         ->with('flash_notice', 'You are successfully logged in.');
-    // }
-    dd($user);
-    
-    // authentication failure! lets go back to the login page
-    // return Redirect::route('login')
-    //     ->with('flash_error', 'Your username/password combination was incorrect.')
-    //     ->withInput();
+Route::group(array('before' => 'auth'), function()
+{
+    Route::resource('categories', 'CategoriesController');
+    Route::resource('data', 'DataController');
+    Route::get('highcharts/{chart_id}', 'HighChartsController@buildHighCharts');
+    Route::resource('charts', 'ChartsController');
 });
 
-/**
-    Account
-*/
-
-Route::get('account/edit', array('as' => 'account', function () { 
-    return View::make('account');
-}))->before('auth');
-
-
-
-// Route::get('make_user', array('as' => 'make_user', function(){
-// 	$username =  strtolower(Input::get('username'));
-// 	User::create([
-// 	        'username' => $username,
-// 	        'password' => Hash::make(Input::get('password')),
-// 	        ]);
-
-// 	$user = User::where('username', '=', $username)->first();
-
-// Auth::login($user);
-// }));
-
-Route::resource('categories', 'CategoriesController');
-Route::resource('data', 'DataController');
-Route::get('highcharts/{chart_id}', 'HighChartsController@buildHighCharts')->before('auth');
-Route::resource('charts', 'ChartsController');
 
